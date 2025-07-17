dc284c27891ad59c65c4a0766eee53215e8c642f
Ryan Doby <ryandoby10@gmail.com>
2025-07-17 11:51:07 -0400

    added the calculater

---
+++ calculater/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main(){
+
+  char op;
+
+  int add();
+
+  int sub();
+
+  printf("Would you like to add or subtract\n");
+
+  printf("enter - or +");
+
+  scanf("%c", &op);
+    
+  if (op == '+') {
+    add();
+  } else if (op == '-') {
+    sub();
+  }
+  
+return 0;  
+}
+
+
+
+
+
+int add(){
+  int x;
+  int y;
+
+ printf("vaule of 1st number\n");
+
+ scanf("%d", &x);
+
+ printf("value of 2nd number\n");
+
+ scanf("%d", &y);
+
+ printf("%d \n", x + y);
+   
+return 0;
+}
+
+
+
+
+
+int sub(){
+
+  int x;
+  int y;
+
+  printf("Value of 1st number");
+
+  scanf("%d", &x);
+
+  printf("Value of 2nd number");
+
+  scanf("%d", &y);
+
+  printf("%d", x - y);
+
+
+  return 0;
+ 
+}
