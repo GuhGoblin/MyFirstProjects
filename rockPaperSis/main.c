cb3bbfac777a8d56e8e3918b098fc1d463096dda
Ryan Doby <ryandoby10@gmail.com>
2025-07-17 14:22:05 -0400

    Making project

---
+++ rockPaperSis/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+  char aiAttack;
+  char scissors[10];
+  char paper[5];
+  char rock[5];
+
+  printf("Rock Paper Scissors");
+
+  printf("Choice your move");
+
+  scanf("%C", paper)
+
+  
+
+}
