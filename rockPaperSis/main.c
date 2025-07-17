41947f1489427c67b944fa551612a3d854dc3422
Ryan Doby <ryandoby10@gmail.com>
2025-07-17 16:42:31 -0400

    Logic for winning is done

--- rockPaperSis/main.c
+++ rockPaperSis/main.c
@@ -1,50 +1,63 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 #include <time.h>
 
+int main(){
 
-  int main(){
+int humanAns;
 
-  int humanAns;
+printf("Rock Paper Scisccors\n");
 
-  int ai();
+printf("1 Rock\n");
+printf("2: Paper\n");
+printf("3: Scisccors\n");
 
-  printf("Rock Paper Scissors\n");
+scanf("%d", &humanAns);
 
-  printf("Choice your move\n");
-
-  printf("1: Rock\n");
-  printf("2: papper\n");
-  printf("3: Scissors\n");
-
-  scanf("%d", &humanAns);
-
-    if (humanAns == 1) {
-    printf("You picked rock\n");
-  } else if(humanAns == 2) {
-    printf("you picked paper\n");
-  } else if(humanAns == 3) {
-    printf("You choice Scissors\n");
-  } 
-  
-  return ai();
+  if(humanAns == 1) {
+  printf("You choice Rock\n");
+} else if(humanAns == 2) {
+  printf("You Choice Paper\n");
+} else if(humanAns == 3) {
+  printf("You picked Scissors\n");
 }
 
+srand(time(NULL));
 
-int ai() {
+int aiAns = rand() %3 +1;
 
-  srand(time(NULL)); 
+printf("Ai's turn\n");
 
-  int aiAns = rand() %3 +1;
+  if(aiAns == 1) {
+  printf("Ai picks Rock\n");
+} else if(aiAns == 2) {
+  printf("ai picks paper\n");
+} else if (aiAns == 3) {
+  printf("Ai picks Scissors\n");
+}
 
-  printf("Ai's turn\n");
+int humanWin = humanAns;
+int aiWin = aiAns;
 
-     if(aiAns == 1) {
-    printf("Ai picks Rock\n");
-  }  else if(aiAns == 2){
-    printf("Ai picks Paper\n");
-   } else if(aiAns == 3){
-    printf("Ai picks Scissors\n");
+if (humanWin == aiWin) {
+  printf("Draw!");
+}
+
+  if(humanWin == 1 && aiWin == 3) {
+    printf("You Win!\n");
+  }else if (humanWin == 2 && aiWin == 1) {
+    printf("You win!\n");
+  }else if(humanWin == 3 && aiWin == 2) {
+    printf("You Win!\n");
   }
+
+  if(aiWin == 1 && humanWin == 3){
+    printf("You lost!\n");
+  }else if(aiWin == 2 && humanWin == 1) {
+    printf("You lost\n");
+  }else if(aiWin == 3 && humanWin == 2){
+    printf("You lost!\n");
+  }
+
   return 0;
 }
