8473feeb37459968aaf2006b3aa8cda45b0b650b
Ryan Doby <ryandoby10@gmail.com>
2025-07-18 11:53:26 -0400

    Added toDo app I'm working on

---
+++ toDO/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+  int choice;
+
+  printf("Welocme to the todo app, what would you like to do\n");
+
+  printf("1: Add task\n");
+
+  printf("2: Veiw Task\n");
+
+  printf("3: Remove task\n");
+
+  printf("4: Exit\n");
+
+  scanf("%d\n", &choice);
+}
